70b4a7740c4555bdd62823c07e981cb03b3dd731
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-05-30 13:42:25 +0600

    create model for the products

--- src/app/modules/products/products.model.ts
+++ src/app/modules/products/products.model.ts
@@ -1,7 +1,36 @@
-import { TProduct } from './products.interface';
+import { TInventory, TProduct, TVariants } from './products.interface';
 import { Schema, model, connect } from 'mongoose';
 
+  const variantsSchema=new Schema <TVariants>({
+     type:{
+        type: String,
+     },
+     value:{
+        type:String
+     }
+  })
 
+  const inventorySchema= new Schema <TInventory>({
+     quantity:{
+        type:Number
+     },
+     inStock:{
+        type:Boolean
+     }
+  })
+
+ 
 const productSchema= new Schema <TProduct>({
-    
+    name:{type:String,required:true},
+    description:{type:String},
+    price:{type:Number,required:true},
+    category:{type:String,required:true},
+    tags:[{type:String}],
+    variants:{type:[variantsSchema]},
+    inventory:inventorySchema
 })
+
+
+/// create model
+ const Product=model<TProduct>("Product",productSchema)
+ 
