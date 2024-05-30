a11e0303ec0a53c8809177ac98e28a1eb59d5ffb
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-05-30 11:44:12 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app:Application = express()
+const port = 3000
+
+// parser
+
+app.use(express.json())
+app.use(cors())
+
+const getAController=(req:Request, res:Response) => {
+  const a=30;
+ res.send(a);
+
+}
+
+app.get('/', getAController)
+
+export default app
