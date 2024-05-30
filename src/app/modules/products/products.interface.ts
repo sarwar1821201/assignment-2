5a4d68f89952f545100cde6d8e9b714557d42323
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-05-30 12:27:57 +0600

    create interface for the products

---
+++ src/app/modules/products/products.interface.ts
@@ -0,0 +1,23 @@
+
+import { Schema, model, connect } from 'mongoose';
+
+  export type TVariants={
+    type: string;
+    value: string;
+ }
+
+  export type TInventory={
+    quantity: number;
+    inStock: boolean;
+  }
+
+export type TProduct={
+    name:string;
+    description:string;
+    price:number;
+    category:string;
+    tags?:string[];
+    variants:[TVariants];
+    inventory:TInventory
+
+}
---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,7 @@
+import { TProduct } from './products.interface';
+import { Schema, model, connect } from 'mongoose';
+
+
+const productSchema= new Schema <TProduct>({
+    
+})
